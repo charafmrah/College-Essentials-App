b6c0b39c0fc3c9bb1776db408c18510760a17b4a
Charaf Mrah <charaf4charaf@gmail.com>
2020-12-26 15:06:53 +0300

    added an empty GPA Calculator page

---
+++ lib/screens/GpaCalculator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GpaCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GPA Calculator'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // code goes here
+          ],
+        ),
+      ),
+    );
+  }
+}
