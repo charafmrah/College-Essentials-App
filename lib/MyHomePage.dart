9c51febcddffeca0a434fffa43202599e149b663
Charaf Mrah <charaf4charaf@gmail.com>
2020-12-27 16:08:53 +0300

    customized the home page buttons

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -5,8 +5,6 @@ import 'package:college_essentials_app/Navigation.dart';
 class MyHomePage extends StatelessWidget {
   final String title = "College Essentials";
 
-  // MyHomePage({Key key, this.title}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,41 +12,46 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
         backgroundColor: Colors.redAccent,
       ),
-      body: Wrap(
-        spacing: 8.0,
-        runSpacing: 4.0,
-        children: <Widget>[
-          RaisedButton(
-            child: new Text("GPA Calculator"),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            onPressed: () => Navigation.navigateToGpaCalculator(context),
-          ),
-          RaisedButton(
-            child: new Text("TO-DO List"),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            onPressed: () => Navigation.navigateToToDoList(context),
-          ),
-          RaisedButton(
-            child: new Text("Notes"),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            onPressed: () => Navigation.navigateToNotes(context),
-          ),
-          RaisedButton(
-            child: new Text("Upcoming Events"),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            onPressed: () => Navigation.navigateToUpcomingEvents(context),
-          ),
-          RaisedButton(
-            child: new Text("Grades Tracker"),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            onPressed: () => Navigation.navigateToGradesTracker(context),
-          ),
-        ],
+      body: Container(
+        width: double.infinity,
+        alignment: Alignment.topCenter,
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 8.0,
+          runSpacing: 4.0,
+          children: <Widget>[
+            RaisedButton(
+              child: new Text("GPA Calculator"),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              onPressed: () => Navigation.navigateToGpaCalculator(context),
+            ),
+            RaisedButton(
+              child: new Text("TO-DO List"),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              onPressed: () => Navigation.navigateToToDoList(context),
+            ),
+            RaisedButton(
+              child: new Text("Notes"),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              onPressed: () => Navigation.navigateToNotes(context),
+            ),
+            RaisedButton(
+              child: new Text("Upcoming Events"),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              onPressed: () => Navigation.navigateToUpcomingEvents(context),
+            ),
+            RaisedButton(
+              child: new Text("Grades Tracker"),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              onPressed: () => Navigation.navigateToGradesTracker(context),
+            ),
+          ],
+        ),
       ),
     );
   }
