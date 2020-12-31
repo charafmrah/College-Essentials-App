50929df0e51c5ab20c2e1ebfbe43e820eb69aa2a
Charaf Mrah <charaf4charaf@gmail.com>
2020-12-31 12:19:32 +0300

    fixed navigation bug

--- lib/Navigation.dart
+++ lib/Navigation.dart
@@ -2,6 +2,7 @@ import 'package:college_essentials_app/MyHomePage.dart';
 import 'package:college_essentials_app/screens/PomodoroTimer.dart';
 import 'package:college_essentials_app/screens/GradesTracker.dart';
 import 'package:college_essentials_app/screens/Notes.dart';
+import 'package:college_essentials_app/screens/PomodoroTimer.dart';
 import 'package:college_essentials_app/screens/ToDoList.dart';
 import 'package:college_essentials_app/screens/UpcomingEvents.dart';
 import 'package:flutter/material.dart';
@@ -37,7 +38,7 @@ class Navigation {
 
   static Future navigateToUpcomingEvents(context) async {
     Navigator.push(
-        context, MaterialPageRoute(builder: (context) => eventspage()));
+        context, MaterialPageRoute(builder: (context) => UpcomingEvents()));
   }
 
   static Future navigateToTimer(context) async {
